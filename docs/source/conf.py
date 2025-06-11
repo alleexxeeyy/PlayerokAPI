e37d8ac19a168940a67c9e62d609fa619c647a71
alleexxeeyy <flizkapodlizka@gmail.com>
2025-06-11 13:12:58 +0500

    11.06.25 тесты и фиксы

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,6 @@ author = "alleexxeeyy"
 
 
 # -- General configuration ---------------------------------------------------
-# -- General configuration
 
 extensions = [
     "sphinx.ext.duration",
@@ -35,27 +34,24 @@ extensions = [
 ]
 
 intersphinx_mapping = {
-    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
     "python": ("https://docs.python.org/3.12/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
 intersphinx_disabled_domains = ["std"]
 
 templates_path = ["_templates"]
 
 # -- Options for EPUB output
-epub_show_urls = "footnote"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
+
 html_permalinks_icon = '<span>#</span>'
 html_theme = 'furo'
 
